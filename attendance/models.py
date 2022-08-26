8cb12b2d7e9c76d226dff2211dbc671620703c66
nien-chihtsai <mammuthus123@gmail.com>
2022-08-26 21:13:04 +1200

    recover

--- attendance/models.py
+++ attendance/models.py
@@ -44,19 +44,12 @@ class Semester(models.Model):
     id = models.IntegerField(primary_key=True)
     year = models.IntegerField()
     semester = models.CharField(max_length=200, null=True, blank=True)
+    # One semester runs one to many courses
+    # One course is run in zero to many semesters
+    course = models.ManyToManyField(Course, related_name='semester_course', blank=False)
 
     def __str__(self):
-        return  self.id.__str__()
-
-
-class Class(models.Model):
-    number = models.IntegerField(primary_key=True)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
-    lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.number.__str__()
+        return self.id.__str__()
 
 
 class CollegeDay(models.Model):
@@ -64,4 +57,24 @@ class CollegeDay(models.Model):
 
     def __str__(self):
         return self.date.__str__()
+
+
+class Class(models.Model):
+    number = models.IntegerField(primary_key=True)
+    # One course can be separated into one to many classes
+    # One class can only run for one course
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='class_course', blank=False)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    # One class can be taught by only one lecturer,
+    # One lecturer teaches zero to many classes
+    lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE, blank=False)
+    # One class holds one to many students’ enrollments
+    # One student fit in one class
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, blank=False)
+    # One college day runs zero to many classes
+    # One class runs on zero to many college days
+    collegeDay = models.ManyToManyField(CollegeDay, related_name='class_collegeDay', blank=True)
+
+    def __str__(self):
+        return self.number.__str__()
 
